235853fcfdac88c2748dc2b129d1328c5ab0d5c2
Luciano Molaro <molarolucianodelongo@gmail.com>
2024-11-08 05:33:29 -0300

    Despliegue

--- Climax.py
+++ Climax.py
@@ -1,9 +1,9 @@
 import datetime
-
 import re
 import secrets
 from flask import Flask, redirect, request, render_template, session, url_for
 import requests
+
 
 api_key = "bdf79b2f2d5637450827abd057f7c1d1"
 
@@ -13,138 +13,13 @@ temperatura = ""
 min_temp = ""
 max_temp = ""
 pais=""
+mensaje=""
 
-
-
-# #-----------------------------------------------------------------------------------------------------------------# 
-# def ugradual():
-#     global grados
-#     print("Ingrese si quiere la medida en sistema metrico o imperial:")
-#     med = input().lower()
-#     if med == "metrico" or med == "metric":
-#         grados = "metric"
-#         print("Sistema elegido: métrico")
-#     elif med == "imperial":
-#         grados = "imperial"
-#         print("Sistema elegido: imperial")
-#     else:
-#         print("Opción no válida, se continuará en el sistema que estaba antes.")
-
-
-# #-----------------------------------------------------------------------------------------------------------------# 
-# def menu():
-#     while True:
-#         print("Seleccione una opción")
-#         print("1) Temperatura")
-#         print("2) Elegir medida de temperatura")
-#         print("3) Pronóstico de los próximos días")
-#         print("4) Mostrar historial de consultas")
-#         opcion = input()
-#         if opcion == "1":
-#             tempminymax()
-#         elif opcion == "2":
-#             ugradual()    
-#         elif opcion == "3":
-#             pronosticoDias()
-#         elif opcion=="4":
-#             mostrarHistorial()    
-#         else:
-#             print("Opción no válida. Saliendo del programa.")
-#             break
-
-
-# #-----------------------------------------------------------------------------------------------------------------#         
-# def guardar_solicitud(consulta, resultado):
-#     with open("Historial.txt", "a") as f:
-#         f.write(f"Consulta: {consulta} - {resultado}\n")
-
-# #-----------------------------------------------------------------------------------------------------------------#         
-# def borrarCaractes(palabra):
-#     return re.sub(r'[^a-zA-Z]', '', palabra)
-
-# #-----------------------------------------------------------------------------------------------------------------#        
-# def tempminymax():
-#     Urlciudad = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units={grados}"
-    
-#     try:
-#         info = requests.get(Urlciudad)
-#         info.raise_for_status()  # Lanza un error si la respuesta fue un error HTTP
-#         infojson = info.json()
-        
-#         temp = infojson["main"]["temp"]
-#         min_temp = infojson["main"]["temp_min"]
-#         max_temp = infojson["main"]["temp_max"]
-        
-#         print(f"La temperatura en {ciudad} es de {temp}")
-#         print(f"La temperatura mínima es de {min_temp}")
-#         print(f"La temperatura máxima es de {max_temp}")
-
-#         guardar_solicitud(ciudad, temp)
-        
-#     except requests.exceptions.HTTPError as http_err:
-#         print(f"Error HTTP: {http_err}")
-    
-#     print("Desea volver al menú? escribiendo 'si', volverá, de lo contrario saldrá del programa")
-#     volver = input().lower()
-#     if volver == "si":
-#         menu()
-#     else:
-#         exit()
-
-
-
-# #------------------------------------------------------------------------------------------------#   
-# def pronosticoDias():
-#    while True:
-#         Urlciudad = f"https://api.openweathermap.org/data/2.5/forecast?q={ciudad}&appid={api_key}&units={grados}"
-#         guardar_solicitud(ciudad, "")
-#         try:
-#             info = requests.get(Urlciudad)
-#             info.raise_for_status()  # Lanza un error si la respuesta fue un error HTTP
-#             infojson = info.json()
-        
-#             for forecast in infojson['list']:
-#                 date = forecast['dt_txt']  
-#                 temperature = forecast['main']['temp']
-#                 description = forecast['weather'][0]['description']
-#                 if date.endswith('12:00:00'):
-#                     print(f"{date}: {temperature}°C, {description}")
-#             break  
-#         except requests.exceptions.HTTPError as http_err:
-#             print(f"Error HTTP: {http_err}")
-       
-
-
-# #------------------------------------------------------------------------------------------------#      
-# def mostrarHistorial():
-#     try:
-#         with open("Historial.txt", "r") as f:
-#             historial = f.readlines()
-#             if historial:
-#                 print("\nHistorial de Consultas:")
-#                 for linea in historial:
-#                     print(linea.strip() + "°C")
-#             else:
-#                 print("El historial está vacío.")
-#     except FileNotFoundError:
-#         print("No se encontró el archivo de historial.")
-
-
-# ciudad=""
-# while not ciudad:
-#     print("Ingrese una ciudad")
-#     ciudad=borrarCaractes(input())
-#     if not ciudad:
-#         print("Debe ingresar una ciudad sin numeros ni caracteres especiales")
-#     elif():
-#         break
-
-
-# menu()
 app=Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 historial = []
 pronosticos = {}
+DatosCiudad = []
 
 
 codigoPaises = {
@@ -207,8 +82,34 @@ dias = {
     'Sunday': 'Domingo'
 }
 
-def borrarCaractes(palabra):
-    return re.sub(r'[^a-zA-Z]', '', palabra)
+def borrarCaractes(ciudad):
+    ciudad = re.sub(r'[^a-zA-Z ]', '', ciudad)
+    ciudad= ciudad.strip()
+    if ciudad:
+        return ciudad
+
+def fueConsultada(ciudad):
+    global DatosCiudad
+    posicion=0
+    for consulta in DatosCiudad:
+        if consulta['ciudad']==ciudad:
+            return True, posicion
+            break
+        posicion+=1
+
+    return False, -2
+    
+
+# def obtenerDatosCiudad(ciudad):
+    
+#     ciudades_guardadas = session.get('Ciudad', [])
+    
+    
+#     for ciudad_guardada in ciudades_guardadas:
+#         if ciudad_guardada['ciudad'].lower() == ciudad.lower():  
+#             return ciudad_guardada
+#         else:
+#             return False
 
 def nombrePais(codigo):
     return codigoPaises.get(codigo,"")
@@ -222,81 +123,166 @@ def gradosEnAbreviatura(grados):
     return abreviatura        
 
 @app.route("/", methods=["GET", "POST"])
-def home():
-    global grados, historial, ciudad, temperatura, max_temp, min_temp, pais
+def inicio():
+    global temperatura, ciudad, pais, max_temp, min_temp, grados, historial, DatosCiudad
     
-    ciudad = request.args.get("ciudad", "")
-    temperatura = request.args.get("temperatura", "")
-    max_temp = request.args.get("max_temp", "")
-    min_temp = request.args.get("min_temp", "")
-    pais = request.args.get("pais", "")
+    ciudad = request.args.get("ciudad")
+    fue=request.args.get("fueConsultada")
+    grados=request.args.get("grados", "metric")
+
     
-    if request.method == "POST":
-        if request.form.get("grados"):
-                grados = request.form.get("grados")  
-        else:
-                ciudad = borrarCaractes(request.form.get("ciudad"))
-        if ciudad:
-            Urlciudad = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units={grados}"
-            try:
-                info = requests.get(Urlciudad)
-                info.raise_for_status()
-                infojson = info.json()
+    
+    if ciudad and fue:
+        DatosCiudad=session.get('Ciudad')
+        posicion=int(request.args.get("posicion"))
+        ciudad=ciudad
+        temperatura=DatosCiudad[posicion]['temperatura']
+        pais=DatosCiudad[posicion]['pais']
+        max_temp=DatosCiudad[posicion]['max_temp']
+        min_temp=DatosCiudad[posicion]['min_temp']
+        gradosAb=DatosCiudad[posicion]['gradosAb']
+        return redirect(url_for('temp', ciudad=ciudad, temperatura=temperatura, pais=pais, max_temp=max_temp, min_temp=min_temp, gradosAb=gradosAb, DatosCiudad=DatosCiudad))
 
-                temperatura = infojson["main"]["temp"]
-                min_temp = infojson["main"]["temp_min"]
-                max_temp = infojson["main"]["temp_max"]
-                codigoPais = infojson["sys"]["country"]
-                pais=nombrePais(codigoPais)
+    elif ciudad:
+        url=f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units={grados}"
+        try:
+            info = requests.get(url)
+            info.raise_for_status()
+            infojson=info.json()
 
-                consulta = {
-                        "fecha": datetime.datetime.now().strftime("%d/%m/%Y"),
-                        "hora" : datetime.datetime.now().strftime("%H:%M"),
-                        "pais": pais.upper(),
-                        "ciudad": ciudad.upper(),
-                        "temp": f"{temperatura}{gradosEnAbreviatura(grados)}"
-                    }
 
-                historial = session.get('historial', [])
-                historial.append(consulta) 
-                session['historial'] = historial 
+            # ciudad = ciudad.lower()
+            # temperatura = infojson['main']['temp']
+            # min_temp = infojson['main']['temp_min']
+            # max_temp = infojson['main']['temp_max']
+            # sensacionTermica=infojson['main']['feels_like']
+            # nubosidad= infojson['weather'][0]['main']
+            # codigoPais = infojson['sys']['country']
+            # pais=nombrePais(codigoPais)
+            # grados=grados
+            # gradosAb=gradosEnAbreviatura(grados)
+            DatosCiudad=session.get('Ciudad', [])
+            datos={
+                "ciudad": ciudad,
+                "temperatura": f"{infojson['main']['temp']:.0f}",
+                "max_temp": f"{infojson['main']['temp_max']:.0f}",
+                "min_temp": f"{infojson['main']['temp_min']:.0f}",
+                "nubosidad": infojson['weather'][0]['main'],
+                "sensacionTermica": infojson['main']['feels_like'],
+                "humedad":infojson['main']['humidity'],
+                "codigoPais": infojson['sys']['country'],
+                "pais": nombrePais(infojson['sys']['country']),
+                "grados": grados,
+                "gradosAb": gradosEnAbreviatura(grados)
+            }
 
-                return redirect(url_for('temp', ciudad=ciudad, temperatura=temperatura, pais=pais, max_temp=max_temp, min_temp=min_temp))
-            except requests.exceptions.HTTPError as http_err:
-                    print("No se encontro ciudad")
+            DatosCiudad.append(datos)
+            session['Ciudad'] = DatosCiudad
 
-    return render_template("index.html", ciudad=ciudad, temperatura=temperatura, min_temp=min_temp, max_temp=max_temp, historial=historial, pais=pais)
+
+            historial = session.get('historial', [])
+            consulta = {
+                "fecha": datetime.datetime.now().strftime("%d/%m/%Y"),
+                "hora" : datetime.datetime.now().strftime("%H:%M"),
+                "pais": pais.upper(),
+                "ciudad": ciudad.upper(),
+                "temp": f"{datos['temperatura']}{gradosEnAbreviatura(grados)}"
+            }
+
+            historial.append(consulta) 
+            session['historial'] = historial
+            return redirect(url_for('temp', ciudad=datos['ciudad'], temperatura=datos['temperatura'], pais=datos['pais'], max_temp=datos['max_temp'], min_temp=datos['min_temp'], gradosAb=datos['gradosAb'], grados=datos['grados'], DatosCiudad=DatosCiudad, nubosidad=datos['nubosidad'], humedad=datos['humedad'], sensacionT=datos['sensacionTermica']))
+        except requests.exceptions.RequestException as e:
+            
+            return render_template("index.html", mensaje="Ciudad no encontrada")
+        except KeyError as e:
+            
+            return render_template("index.html", mensaje="No se encontró la información completa de la ciudad")
+        except Exception as e:
+
+            return render_template("index.html", mensaje="Ocurrió un error inesperado")
+    return render_template("index.html")
+
+@app.route("/validacion", methods=["POST"])
+def validarCiudad():
+    global DatosCiudad
+    ciudad = request.form.get("ciudad").lower()
+    ciudad = borrarCaractes(ciudad)
+    
+
+    if ciudad:
+            fue, posicion=fueConsultada(ciudad)
+            if fue:
+                return redirect (url_for('inicio', ciudad=ciudad, fueConsultada=fue, posicion=posicion))
+            else:
+                return redirect (url_for('inicio', ciudad=ciudad))
+    else:
+        return render_template("index.html", mensaje="Ciudad invalida")
+
 
 @app.route("/clima")
 def temp():
-    ciudad = request.args.get("ciudad", "")
-    temperatura = request.args.get("temperatura", "")
-    pais = request.args.get("pais", "")
-    maxtemp = request.args.get("max_temp", "")
-    mintemp = request.args.get("min_temp","")
-    gradosAb = gradosEnAbreviatura(grados) 
-    return render_template("clima.html", ciudad=ciudad.upper(), temperatura=temperatura, pais=pais, maxtemp=maxtemp, mintemp=mintemp, grados=gradosAb)
+
+
+
+
+    ciudad = request.args.get('ciudad')
+    temperatura = request.args.get('temperatura')
+    pais = request.args.get('pais')
+    maxtemp = request.args.get('max_temp')
+    mintemp = request.args.get('min_temp')
+    gradosAb = request.args.get('gradosAb')
+    nubosidad = request.args.get('nubosidad')
+    humedad=request.args.get('humedad')
+    sensacionT=request.args.get('sensacionT')
+   
+    return render_template("clima.html", ciudad=ciudad.upper(), temperatura=temperatura, pais=pais, maxtemp=maxtemp, mintemp=mintemp, gradosAb=gradosAb, nubosidad=nubosidad, humedad=humedad, sensacionT=sensacionT)
 
 @app.route("/cambiar_unidad", methods=["POST"])
-def cambiar_unidad():
-    global grados,historial, ciudad, temperatura, max_temp, min_temp, pais  
+def cambiar_unidad(): 
+    global grados, DatosCiudad
+    DatosCiudad=session.get('Ciudad')
+    gradosActual = DatosCiudad[-1]['grados']
     grados = request.form.get("grados")
+    temperatura=float(DatosCiudad[-1]['temperatura'])
+    ciudad=DatosCiudad[-1]['ciudad']
+    pais=DatosCiudad[-1]['pais']
+    min_temp=float(DatosCiudad[-1]['max_temp'])
+    max_temp=float(DatosCiudad[-1]['min_temp'])
+    humedad=DatosCiudad[-1]['humedad']
+    nubosidad=DatosCiudad[-1]['nubosidad']
+    sensacionT=float(DatosCiudad[-1]['sensacionTermica'])
     
-    if grados=="metric":
-         temperatura=temperatura
-         min_temp=min_temp
-         max_temp=max_temp
-    elif grados=="imperial":
-        temperatura = temperatura+33.8 
-        min_temp = min_temp+33.8
-        max_temp = max_temp+33.8       
+
+    if grados=="metric" and gradosActual!="metric":
+         temperatura=(temperatura-32)*5/9
+         min_temp=(min_temp-32)*5/9
+         max_temp=(max_temp-32)*5/9
+         sensacionT=(sensacionT-32)*5/9
+    elif grados=="imperial" and gradosActual!="imperial":
+        temperatura = (temperatura*9/5)+32
+        min_temp = (min_temp*9/5)+32
+        max_temp = (max_temp*9/5)+32
+        sensacionT=(sensacionT*9/5)+32
     
-    return redirect(url_for('temp', ciudad=ciudad.upper(), temperatura=temperatura, pais=pais, max_temp=max_temp, min_temp=min_temp, grados=gradosEnAbreviatura(grados)))
+    DatosCiudad[-1]['grados']=grados
+    DatosCiudad[-1]['temperatura']=temperatura
+    DatosCiudad[-1]['max_temp']=max_temp
+    DatosCiudad[-1]['min_temp']=min_temp
+    DatosCiudad[-1]['sensacionTermica']=sensacionT
+
+    temperatura=f"{temperatura:.0f}"
+    max_temp=f"{max_temp:.0f}"
+    min_temp=f"{min_temp:.0f}"
+    sensacionT=f"{sensacionT:.2f}"
+
+    return redirect(url_for('temp', ciudad=ciudad.upper(), pais=pais, temperatura=temperatura,max_temp=max_temp, min_temp=min_temp, gradosAb=gradosEnAbreviatura(grados), nubosidad=nubosidad, humedad=humedad, sensacionT=sensacionT))
 
 @app.route('/historial')
 def mostrar_historial():
     global historial, ciudad, temperatura, max_temp, min_temp, pais  
-    return render_template("historial.html", historial=historial, ciudad=ciudad.upper(), temperatura=temperatura, pais=pais, max_temp=max_temp, min_temp=min_temp)
+    return render_template("historial.html", historial=historial)
+
 
 @app.route('/pronosticos')
 def mostrar_pronosticos():
@@ -316,9 +302,9 @@ def mostrar_pronosticos():
             fecha = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
             dia_ingles = fecha.strftime('%A')
             dia = dias[dia_ingles]
-            temperature = forecast['main']['temp']
-            min_temp=forecast['main']['temp_min']
-            max_temp=forecast['main']['temp_max']
+            temperature = f"{forecast['main']['temp']:.0f}"
+            min_temp=f"{forecast['main']['temp_min']:.0f}"
+            max_temp=f"{forecast['main']['temp_max']:.0f}"
             description = forecast['weather'][0]['description']
             if date.endswith('12:00:00'):
                 pronosticos[ciudad].append({
